41aa411759e1f805bb30911bfd90bdaf04b4811d
Bendi71 <bendeguz.horvath2@stud.uni-corvinus.hu>
2023-02-24 12:22:18 +0100

    .

--- Backtrader/Bt_stock_frame.py
+++ Backtrader/Bt_stock_frame.py
@@ -1,26 +1,24 @@
 import datetime
-import my_indicators as id
+import my_indicators as ind
 import backtrader as bt
 import yfinance as yf
 import Bt_strats as st
 import matplotlib.pyplot as plt
 
+# inputs
+ticker = input("Ticker: ")
+startdate = input("Start date: ")
+closedate = input("End date: ")
 
-#inputs
-ticker=input("Ticker: ")
-startdate=input("Start date: ")
-closedate=input("End date: ")
+cerebro = bt.Cerebro()
 
-
-cerebro=bt.Cerebro()
-
-#add data feed
-df=yf.download(ticker, start=startdate, end=closedate,interval='1d', auto_adjust=True, ignore_tz=True)
-data=bt.feeds.PandasData(dataname=df)
+# add data feed
+df = yf.download(ticker, start=startdate, end=closedate, interval='1d', auto_adjust=True, ignore_tz=True)
+data = bt.feeds.PandasData(dataname=df)
 cerebro.adddata(data)
 
-#add strategy
-strat=st.RSIStrategy
+# add strategy
+strat = st.SMACrossStrategy
 cerebro.addstrategy(strat)
 
 # Set the cash, commission, stake size, slippage
@@ -53,21 +51,21 @@ win_rate = (total_wins / total_trades) * 100
 # Print performance metrics
 print('Total Return:', round(returns * 100, 2), '%')
 print('Annual Return:', round(annual_returns * 100, 2), '%')
-print('Sharpe Ratio:', round(sharpe_ratio, 2))
+print('Sharpe Ratio: %.2f' % sharpe_ratio)
 print('Max Drawdown: %.2f%%' % drawdown['max']['drawdown'])
 print('Total Trades:', total_trades)
 print('Total Wins:', total_wins)
 print('Total Losses:', total_losses)
 print('Win Rate: %.2f%%' % win_rate)
 
-#Plotting
+# Plotting
 dates = [datetime.date.fromordinal(int(tim)) for tim in data.datetime.array]
 buy_times = strat.buy_times
 sell_times = strat.sell_times
 closes = data.close.array
 
 if strat == st.RSIStrategy:
-    fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, figsize=(12,8))
+    fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, figsize=(12, 8))
 
     # First subplot for buy/sell signals and closing price
     ax1.scatter(buy_times, strat.buy_prices, marker='^', color='green', label='Buy', s=100)
@@ -77,13 +75,12 @@ if strat == st.RSIStrategy:
     ax1.set_title('MSFT')
 
     # Second subplot for RSI
-    rsi = id.rsi(df['Close'],periods=10)
+    rsi = ind.rsi(df['Close'], periods=10)
     ax2.plot(dates, rsi, color='blue')
     ax2.axhline(65, color='red', linestyle='dotted')
     ax2.axhline(35, color='green', linestyle='dotted')
     ax2.set_ylabel('RSI')
     ax2.set_xlabel('Date')
-    plt.show()
 elif strat == st.SMACrossStrategy:
     plt.plot(dates, closes, color='black', alpha=0.5)
     plt.ylabel('Close Price')
@@ -92,12 +89,26 @@ elif strat == st.SMACrossStrategy:
     plt.scatter(sell_times, strat.sell_prices, marker='v', color='red', label='Sell', s=100)
     plt.plot(dates, df['Close'].rolling(window=30).mean(), color='blue')
     plt.plot(dates, df['Close'].rolling(window=10).mean(), color='red')
-    plt.show()
 elif strat == st.DonchianBreakout:
+    plt.axes().set_facecolor('black')
+    plt.plot(dates, closes, color='white', alpha=0.5)
+    plt.ylabel('Close Price')
+    plt.title('MSFT')
+    plt.scatter(buy_times, strat.buy_prices, marker='^', color='green', label='Buy', s=100)
+    plt.scatter(sell_times, strat.sell_prices, marker='v', color='red', label='Sell', s=100)
+    plt.plot(dates, ind.donchianchannel(df, 20)['upper'], color='darkcyan')
+    plt.plot(dates, ind.donchianchannel(df, 20)['lower'], color='darkcyan')
+    plt.plot(dates, ind.donchianchannel(df, 20)['mid'], color='lightcyan')
+elif strat == st.TrendFollowingStrategy:
     plt.plot(dates, closes, color='black', alpha=0.5)
     plt.ylabel('Close Price')
     plt.title('MSFT')
     plt.scatter(buy_times, strat.buy_prices, marker='^', color='green', label='Buy', s=100)
     plt.scatter(sell_times, strat.sell_prices, marker='v', color='red', label='Sell', s=100)
-    plt.plot(dates, id.donchianchannel(df,20), color='blue')
-    plt.show()
+elif strat == st.MomentumStrategy:
+    plt.plot(dates, closes, color='black', alpha=0.5)
+    plt.ylabel('Close Price')
+    plt.title('MSFT')
+    plt.scatter(buy_times, strat.buy_prices, marker='^', color='green', label='Buy', s=100)
+    plt.scatter(sell_times, strat.sell_prices, marker='v', color='red', label='Sell', s=100)
+plt.show()
--- Backtrader/Bt_strats.py
+++ Backtrader/Bt_strats.py
@@ -1,4 +1,5 @@
 import backtrader as bt
+from my_indicators import DonchianChannels
 
 
 class SMACrossStrategy(bt.Strategy):
@@ -37,7 +38,6 @@ class SMACrossStrategy(bt.Strategy):
             else:
                 self.sell_times.append(self.datas[0].datetime.datetime())
                 self.sell_prices.append(order.executed.price)
-            self.bar_executed = len(self)
         self.order = None
 
 
@@ -80,6 +80,7 @@ class RSIStrategy(bt.Strategy):
             if self.sell_sig[0] == 1:
                 self.order = self.sell()
 
+
 class DonchianBreakout(bt.Strategy):
     params = (('period', 20), ('printlog', True))
 
@@ -89,7 +90,7 @@ class DonchianBreakout(bt.Strategy):
     sell_prices = []
 
     def __init__(self):
-        self.dc = bt.Indicator.DonchianChannels(self.data, period=self.params.period)
+        self.dc = DonchianChannels(self.data, period=20)
         self.order = None
 
     def notify_order(self, order):
@@ -108,8 +109,93 @@ class DonchianBreakout(bt.Strategy):
         if self.order:
             return
         if not self.position:
-            if self.data.close[0] > self.dc.lines.top[0]:
+            if self.data.close[0] > self.dc.l.dch[0]:
                 self.order = self.buy()
         else:
-            if self.data.close[0] < self.dc.lines.bot[0]:
+            if self.data.close[0] < self.dc.l.dcl[0]:
+                self.order = self.sell()
+
+
+class TrendFollowingStrategy(bt.Strategy):
+    params = (('period', 10), ('printlog', True))
+
+    buy_times = []
+    buy_prices = []
+    sell_times = []
+    sell_prices = []
+
+    def __init__(self):
+        self.order = None
+        self.period = self.params.period
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.buy_times.append(self.datas[0].datetime.datetime())
+                self.buy_prices.append(order.executed.price)
+            else:
+                self.sell_times.append(self.datas[0].datetime.datetime())
+                self.sell_prices.append(order.executed.price)
+        self.order = None
+
+    def next(self):
+        if self.order:
+            return
+
+        if not self.position:
+            if self.data.close[0] > self.data.close[-self.period]:
+                self.order = self.buy()
+        else:
+            if self.data.close[0] < self.data.close[-self.period]:
+                self.order = self.sell()
+
+
+class MomentumStrategy(bt.Strategy):
+    params = (
+        ('sma_period', 50),
+        ('rsi_period', 14),
+        ('rsi_upper', 70),
+        ('rsi_lower', 30),
+        ('macd_fast', 12),
+        ('macd_slow', 26),
+        ('macd_signal', 9),
+        ('printlog', True)
+    )
+
+    buy_times = []
+    buy_prices = []
+    sell_times = []
+    sell_prices = []
+
+    def __init__(self):
+        self.sma = bt.indicators.SMA(self.data, period=self.params.sma_period)
+        self.rsi = bt.indicators.RSI(self.data, period=self.params.rsi_period)
+        self.macd = bt.indicators.MACD(self.data, period_me1=self.params.macd_fast, period_me2=self.params.macd_slow,
+                                       period_signal=self.params.macd_signal)
+        self.buy_sig = bt.indicators.CrossUp(self.data, self.sma)
+        self.sell_sig = bt.indicators.CrossDown(self.data, self.sma)
+        self.order = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.buy_times.append(self.datas[0].datetime.datetime())
+                self.buy_prices.append(order.executed.price)
+            else:
+                self.sell_times.append(self.datas[0].datetime.datetime())
+                self.sell_prices.append(order.executed.price)
+        self.order = None
+
+    def next(self):
+        if self.order:
+            return
+        if not self.position:
+            if self.buy_sig[0] == 1 and self.rsi < self.params.rsi_upper and self.macd.macd[0] > self.macd.signal[0]:
+                self.order = self.buy()
+        else:
+            if self.sell_sig[0] == 1 and self.rsi > self.params.rsi_lower and self.macd.macd[0] < self.macd.signal[0]:
                 self.order = self.sell()
