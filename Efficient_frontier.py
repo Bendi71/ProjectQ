Fixed stock returns formula

Efficient_frontier.py
@@ -14,10 +14,11 @@ if weights.size == 0:
 
 def GetData(stock, time):  # Downloads historical stock prices from yahoofinance and does basic statistical calculations
     hist = yf.download(tickers=stock, period=time, interval='1d', auto_adjust=True, ignore_tz=True)['Close']
-    dailyreturns = hist.pct_change()
+    dailyreturns = np.log(hist / hist.shift(1)).dropna()  # Daily log returns
     meanreturn = dailyreturns.mean()
     covmatrix = dailyreturns.cov()  # Covariance matrix
-    Returns = (hist.tail(1).iloc[0] - hist.head(1).iloc[0]) / hist.head(1).iloc[0]  # Return of the stocks from start
+    Returns = np.log((hist.tail(1).iloc[0] - hist.head(1).iloc[0]) / hist.head(1).iloc[0])  # Return of the stocks from
+    # start
     # to end date
     return Returns, covmatrix  # Output is an array of arrays
 
@@ -69,11 +70,11 @@ def OptimalPortfolio(returns, covmatrix, targetreturn):
 #def OptimalPortfolio(results, covmatrix, riskfreerate = 0,):
 
 
-ret, covmat = GetData(stocks,'2y')
+ret, covmat = GetData(stocks,'10y')
 
 maxSRret, maxSRvar = PortPerformance(maxSharpeRatio(ret,covmat).x,ret,covmat)
 minVarret, minVarvar= PortPerformance(minPortfolioVariance(ret,covmat).x, ret, covmat)
-targetreturns=np.linspace(minVarret,maxSRret,10)
+targetreturns=np.linspace(minVarret,maxSRret,40)
 efficientList = []
 for target in targetreturns:
     data=[target,OptimalPortfolio(ret, covmat, target).fun]

VaR/MC_VaR_port.py
@@ -13,7 +13,7 @@ if weights.size == 0:
 
 def GetData(stock, time):
     hist = yf.download(tickers=stock, period=time, interval='1d', auto_adjust=True, ignore_tz=True)['Close']
-    hozam = hist.pct_change()
+    hozam = np.log(hist / hist.shift(1)).dropna()  # Daily log returns
     meanreturn = hozam.mean()
     covmatrix = hozam.cov()
     return meanreturn, covmatrix, np.array(hist.tail(1))

VaR/MC_VaR_stock.py
@@ -8,7 +8,7 @@ stock = input('Ticker: ')
 
 def GetData(stock):
     hist = yf.download(tickers=stock, period='2y', interval='1d', auto_adjust=True, ignore_tz=True)['Close']
-    hozam = hist.pct_change()
+    hozam = np.log(hist / hist.shift(1)).dropna()  # Daily log returns
     atlag = np.mean(hozam)
     sig = np.std(hozam)
     return atlag, sig, hist[-1]

VaR/VaR_portfolio.py
@@ -13,10 +13,11 @@ if weights.size == 0:
 
 def GetData(stock, time):  # Downloads historical stock prices from yahoofinance and does basic statistical calculations
     hist = yf.download(tickers=stock, period=time, interval='1d', auto_adjust=True, ignore_tz=True)['Close']
-    hozam = hist.pct_change()
+    hozam = np.log(hist / hist.shift(1)).dropna()  # Daily log returns
     meanreturn = hozam.mean()
     covmatrix = hozam.cov()  # Covariance matrix
-    Returns = (hist.tail(1).iloc[0] - hist.head(1).iloc[0]) / hist.head(1).iloc[0]  # Return of the stocks from start
+    Returns = np.log((hist.tail(1).iloc[0] - hist.head(1).iloc[0]) / hist.head(1).iloc[0]) # Return of the stocks from
+    # start
     # to end date
     return hozam.dropna(), Returns, meanreturn, covmatrix, np.array(hist.tail(1))  # Output is an array of arrays
 

VaR/VaR_stock.py
@@ -8,11 +8,11 @@ stock = input('Ticker: ')  # Input is a ticker of a stock
 
 def GetData(stock, time):  # Downloads historical stock prices from yahoofinance and does basic statistical calculations
     hist = yf.download(tickers=stock, period=time, interval='1d', auto_adjust=True, ignore_tz=True)['Close']
-    returns = hist.pct_change()
+    returns = np.log(hist / hist.shift(1)).dropna()  # Daily log returns
     mean_return = np.mean(returns)
     sig = np.std(returns)  # Standard deviation is returns related
     maxdraw = np.min(returns)
-    Return = (hist[-1] - hist[0]) / hist[0]  # Return of the stock from start to end date
+    Return = np.log((hist[-1] - hist[0]) / hist[0])  # Return of the stock from start to end date
     return returns, Return, mean_return, sig, maxdraw, hist[-1]  # Output is an array of values
 
 

VaR/hist_VaR_port.py
@@ -12,9 +12,9 @@ if weights.size == 0:
 
 def GetData(stock, time):
     hist = yf.download(tickers=stock, period=time, interval='1d', auto_adjust=True, ignore_tz=True)['Close']
-    hozam = hist.pct_change()
+    hozam = np.log(hist / hist.shift(1)).dropna() # Daily log returns
     covmat = hozam.cov()
-    Returns = (hist.tail(1).iloc[0] - hist.head(1).iloc[0]) / hist.head(1).iloc[0]
+    Returns = np.log((hist.tail(1).iloc[0] - hist.head(1).iloc[0]) / hist.head(1).iloc[0])
     return hozam.dropna(), Returns, covmat
 
 

VaR/hist_VaR_stock.py
@@ -7,11 +7,11 @@ stock = input('Ticker: ')
 
 def GetData(stock, time):
     hist = yf.download(tickers=stock, period=time, interval='1d', auto_adjust=True, ignore_tz=True)['Close']
-    hozam = hist.pct_change()
+    hozam = np.log(hist / hist.shift(1)).dropna() # Daily log returns
     atlag = np.mean(hozam)
     var = np.std(hozam)
     maxdraw = np.min(hozam)
-    osszhozam = (hist[-1] - hist[0]) / hist[0]
+    osszhozam = np.log((hist[-1] - hist[0]) / hist[0])
     return hozam, osszhozam, atlag, var, maxdraw
 
 